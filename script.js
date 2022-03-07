e79173bfa8f5e00014edbc5205c7acbf91c04c49
Vinicius A.K.A. Akahige <83471044+viniciusbernardo22@users.noreply.github.com>
2022-03-07 15:36:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const LinksSocialMedia = {
+  github: 'viniciusbernardo22',
+  youtube: 'viniciusb',
+  facebook: 'vinicius.bernardo.16',
+  instagram: 'vini.333',
+  twitter: 'vinao'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+    console.log(li.children[0].href)
+  }
+} //changeSocialMediaLinks()
+
+function getUserDataGit() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImg.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getUserDataGit()
